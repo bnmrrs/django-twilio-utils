17c1bb5927c844551ebc4f3f83f9fbf223f24807
bmorris <ben@bnmrrs.com>
2010-08-18 11:31:55 -0400

    Added setup.py for installing

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+#  The MIT License
+#
+#  Copyright (c) 2010 Ben Morris
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+
+
+import distutils.core
+import sys
+# Importing setuptools adds some features like "setup.py develop", but
+# it's optional so swallow the error if it's not there.
+try:
+    import setuptools
+except ImportError:
+    pass
+
+distutils.core.setup(
+    name="django-twilio-utils",
+    version="0.1.1",
+    packages = ["django_twilio_utils"],
+    author="Ben Morris",
+    author_email="ben@bnmrrs.com",
+    url="http://www.github.com/bnmrrs/django-twilio-utils",
+    license="http://www.opensource.org/licenses/mit-license.php",
+    description="A set of utilities for interacting with Twilio from within Django ",
+)
