4248f8fa5fd66162fadff660af899f4a11a33bb1
bmorris <ben@bnmrrs.com>
2010-09-01 19:13:33 -0400

    Added comments to the tests

--- tests/core/tests.py
+++ tests/core/tests.py
@@ -23,18 +23,33 @@ class TwilioRequestDecoratorTest(TestCase):
 		
 class TwilioResponseTest(TestCase):
 	def test_basic_usage(self):
+		# Very basic usage with no verbs
 		r = TwilioResponse()
 		self.failUnlessEqual(str(r), 
 """Content-Type: application/xml
 
 <?xml version='1.0'?><Response></Response>""")
 
+		# Basic normal usage.  Should just have a say verb
 		r = TwilioResponse().say('hello')
 		self.failUnlessEqual(str(r),
 """Content-Type: application/xml
 
 <?xml version='1.0'?><Response><Say voice='woman' loop='0' language='en'>hello</Say></Response>""")
 
+		# Slightly more advanced usage.  Uses method chaining and all
+		# currently implemented methods
+		r = TwilioResponse().play(
+			'hello.mp3'
+		).record(
+			'http://example.org/recording_callback'
+		).gather(
+			'http://example.org/gather_callback'
+		).hangup()
+		self.failUnlessEqual(str(r),
+"""Content-Type: application/xml
+
+<?xml version='1.0'?><Response><Play loop='0'>hello.mp3</Play><Record action='http://example.org/recording_callback' maxLength='60' finishOnKey='#' timeout='5' method='POST' playBeep='false' /><Gather action='http://example.org/gather_callback' numDigits='1' finishOnKey='#' timeout='5' method='POST' /><Hangup /></Response>""")
 
 def test_nested(self):
 	pass
