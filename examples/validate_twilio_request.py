f8b445a2c286b90f3d6b1fc01ca4a9673e5e188a
bmorris <ben@bnmrrs.com>
2010-09-01 19:15:07 -0400

    Added comments to example

--- examples/validate_twilio_request.py
+++ examples/validate_twilio_request.py
@@ -2,9 +2,9 @@
 TWILIO_ACCOUNT_SID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 TWILIO_ACCOUNT_TOKEN = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
 
-#views.py
+# views.py
 from django_twilio_utils.decorators import twilio_request
 
-@twilio_request
+@twilio_request # Validate that the request did come from Twilio
 def twilio_callback(request):
 	# view_code_here
